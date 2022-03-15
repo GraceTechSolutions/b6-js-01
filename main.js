2c8d08f05b5dd7bb6be95ce2dbf3ad3d790d53d0
JRudransh <JRudransh@protonmail.com>
2022-03-15 20:55:42 +0530

    JS Started

---
+++ main.js
@@ -0,0 +1,58 @@
+/*
+document.write('HELLO')
+document.write('HELLO')
+document.write('HELLO')
+document.write('HELLO')
+document.write('HELLO')
+*/
+
+// 1. case sensitive
+// aa
+// AA
+// 2. contains numbers alpbahets _
+// 3. can't start from number
+// 4. can't contain space
+// 5. avoid reserved keywords
+
+// let
+// var
+
+let name = 'JAY'
+var amount = 200000
+
+// let name = 'Rahul'
+var age = 30
+
+// const
+const PI = 3.1416
+let marks = 20
+
+{
+    let marks = 98
+    var res = 'A+'
+    console.log(marks);
+}
+
+// datatypes
+// Number
+let n = 1
+let n2 = 1.5
+let n3 = 0xa
+n4 = NaN
+
+// String
+s1 = 'Hello'
+s2 = "World"
+s3 = `${s1} JavaScript`
+
+s4 = "Don\'t do \t this"
+
+// Boolean
+b1 = true
+b2 = false
+
+// null
+n1 = null
+
+// undefined
+let u1;
